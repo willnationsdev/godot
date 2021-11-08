3d0b9eadab48920838167b1a6ec358b2fef1a54d
Dennis Ploeger <develop@dieploegers.de>
2021-11-08 12:19:55 +0100

    Added integration script tests for anonymous extension

---
+++ modules/gdscript/tests/scripts/runtime/features/anonymous_extend_user.gd
@@ -0,0 +1,5 @@
+extends "/Users/dennis.ploeger/code/private/godot/godot/modules/gdscript/tests/scripts/runtime/features/anonymous_extend.gd"
+class_name TestAnonymousExtend
+
+func test():
+    print("OK")
